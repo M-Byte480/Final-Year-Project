efbe633584104505e9b56e38ea5e810bad499f6f
Milan <kmilanm13@gmail.com>
2024-09-14 00:46:09 +0100

    [Milan] Added Material, Login page complete

--- frontend/src/app/components/login-page/login-page.component.ts
+++ frontend/src/app/components/login-page/login-page.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatIconButton} from "@angular/material/button";
+import {MatIcon, MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormField,
+    MatInput,
+    MatIconButton,
+    MatIcon,
+    MatLabel,
+    MatIconModule,
+    MatSuffix
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+  hide = signal<boolean>(true);
 
+  passwordClickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.warn(this.loginForm.value);
+  }
 }
