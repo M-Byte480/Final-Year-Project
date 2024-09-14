9c85415713e71d883320ef71caaae60664dab092
Milan <kmilanm13@gmail.com>
2024-09-14 01:13:41 +0100

    [Milan] Simple Registration added

--- frontend/src/app/components/registration-page/registration-page.component.ts
+++ frontend/src/app/components/registration-page/registration-page.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
 
 @Component({
   selector: 'app-registration-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormField,
+    MatInput,
+    MatLabel
+  ],
   templateUrl: './registration-page.component.html',
   styleUrl: './registration-page.component.css'
 })
 export class RegistrationPageComponent {
+  registrationForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl('')
+  });
 
+  onSubmit(): void {
+    console.warn(this.registrationForm.value);
+  }
 }
