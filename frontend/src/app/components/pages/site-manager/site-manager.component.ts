6c2dc4726cb9ee5c1d432e05e7f46bddff1e7cea
Milan <kmilanm13@gmail.com>
2024-12-31 22:49:01 +0000

    [Milan] Overview design complete

--- frontend/src/app/components/pages/site-manager/site-manager.component.ts
+++ frontend/src/app/components/pages/site-manager/site-manager.component.ts
@@ -1,31 +1,44 @@
-import {Component} from '@angular/core';
-import {RouterLink} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {Router, RouterLink} from "@angular/router";
 import {Site} from "../../../shared/data-types";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-site-manager',
   standalone: true,
   imports: [
     RouterLink,
-    NgForOf
+    NgForOf,
+    NgOptimizedImage
   ],
   templateUrl: './site-manager.component.html',
   styleUrl: './site-manager.component.css'
 })
-export class SiteManagerComponent {
+export class SiteManagerComponent implements OnInit{
 
-  listOfUserSites: Site[] = [
-    {
-      name: 'milanify',
-      description: '',
-      url: '',
-      id: '1234567890'
-    }
-  ];
+  listOfUserSites: Site[] = [];
+
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
     // Retrieve the list of site names from the backend, using the JWT token
     console.log('Site Manager Component init');
+    for (let i = 0; i < 4; i++) {
+      this.listOfUserSites.push({
+        name: 'Milanify',
+        description: '',
+        url: '',
+        id: '1234567890'
+      })
+    }
   }
+
+  protected onSiteClick(siteId: string){
+    console.log('Site clicked: ' + siteId);
+    this.router.navigate(['/overview', siteId]).then(r => {});
+  }
+
+  protected onCreateNewSite(){}
+
 }
