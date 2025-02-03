c93b096a30019f04c0c9bd911cb51fb74f4065a1
Milan <kmilanm13@gmail.com>
2025-02-03 01:05:05 +0000

    [Milan] Preview

--- frontend/src/app/components/pages/preview/preview-page/preview-page.component.ts
+++ frontend/src/app/components/pages/preview/preview-page/preview-page.component.ts
@@ -2,12 +2,16 @@ import {Component, OnInit} from '@angular/core';
 import {DesignerStateServiceService} from "../../../../services/states/designer-service/designer-state-service.service";
 import {RootComponent} from "../../../../shared/data-types";
 import {PageRendererComponent} from "./page-renderer/page-renderer.component";
+import {NavbarRendererComponent} from "../../../shared/navbar-renderer/navbar-renderer.component";
+import {FooterRendererComponent} from "../../../shared/footer-renderer/footer-renderer.component";
 
 @Component({
   selector: 'app-preview-page',
   standalone: true,
   imports: [
-    PageRendererComponent
+    PageRendererComponent,
+    NavbarRendererComponent,
+    FooterRendererComponent
   ],
   templateUrl: './preview-page.component.html'
 })
