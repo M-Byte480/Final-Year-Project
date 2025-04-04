6f5ccc2a58eeed30dfdfe725197b2aee55656825
Milan <kmilanm13@gmail.com>
2025-04-04 14:49:56 +0100

    [Milan] RECOMPILE

--- backend/src/main/java/milan/backend/configs/WebConfig.java
+++ backend/src/main/java/milan/backend/configs/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://www.milan-kovacs.ie", "https://milan.ie")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
