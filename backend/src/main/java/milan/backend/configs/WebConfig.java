e2bdb6adfd503f0f2ae1deefd30851732b78a3c9
Milan <kmilanm13@gmail.com>
2025-04-04 16:47:45 +0100

    [Milan] gttgethethhhthrthryhrhrtshtdgfzdafrrhsrtedrg

--- backend/src/main/java/milan/backend/configs/WebConfig.java
+++ backend/src/main/java/milan/backend/configs/WebConfig.java
@@ -9,10 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://www.milan-kovacs.ie", "https://www.milan.ie")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+//        registry.addMapping("/**")
+//                .allowedOrigins("https://www.milan-kovacs.ie", "https://www.milan.ie")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
     }
 }
