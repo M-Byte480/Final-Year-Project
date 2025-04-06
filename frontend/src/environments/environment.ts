b4166cd1272012b0c4536a232e68d164cd8f66ff
Milan <kmilanm13@gmail.com>
2025-04-07 00:41:12 +0100

    [Milan] =€

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,12 @@
+// export const environment = {
+//   dev: false,
+//   test: "env.ts",
+//   apiUrl: 'http://localhost:8080', //'https://api.milan-kovacs.ie', //
+//   siteUrl: 'http://localhost:4200/'
+// };
 export const environment = {
   dev: false,
-  test: "env.ts",
-  apiUrl: 'http://localhost:8080', //'https://api.milan-kovacs.ie', //
-  siteUrl: 'http://localhost:4200/'
+  test: "env.prod.ts",
+  apiUrl: 'https://api.milan-kovacs.ie',
+  siteUrl: 'https://www.milan.ie/'
 };
