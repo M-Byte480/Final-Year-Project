bfdaa7db96ac5a47fdd113c9861f87eb17ae8d6e
Milan <kmilanm13@gmail.com>
2025-04-06 22:25:06 +0100

    [Milan] UI revamp complete

--- frontend/src/app/components/pages/login-page/login-page.component.ts
+++ frontend/src/app/components/pages/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import {Component, signal} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
-import {MatIconButton} from "@angular/material/button";
+import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatIcon, MatIconModule} from "@angular/material/icon";
 import {HttpApiService} from "../../../services/http/http-api.service";
 import {ENDPOINTS} from "../../../services/http/endpoints";
@@ -13,16 +13,17 @@ import {Router} from "@angular/router";
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    MatFormField,
-    MatInput,
-    MatIconButton,
-    MatIcon,
-    MatLabel,
-    MatIconModule,
-    MatSuffix
-  ],
+    imports: [
+        ReactiveFormsModule,
+        MatFormField,
+        MatInput,
+        MatIconButton,
+        MatIcon,
+        MatLabel,
+        MatIconModule,
+        MatSuffix,
+        MatButton
+    ],
   templateUrl: './login-page.component.html'
 })
 export class LoginPageComponent {
