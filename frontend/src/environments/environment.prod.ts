1706ad395541b290fab925b1fa9f360bbc54cf60
Milan <kmilanm13@gmail.com>
2025-04-07 00:05:25 +0100

    [Milan] Pipeline configuration

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   dev: false,
   test: "env.prod.ts",
-  apiUrl: 'http://95.216.138.150:8080',
+  apiUrl: 'https://api.milan-kovacs.ie',
   siteUrl: 'https://www.milan.ie/'
 };
