e9492f4427582c030916188f35366d9f44d033df
Milan <77645654+M-Byte480@users.noreply.github.com>
2025-07-15 04:10:01 +0100

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   dev: false,
   test: "env.prod.ts",
-  apiUrl: 'https://api.milan-kovacs.ie',
+  apiUrl: 'https://api.milan.ie',
   siteUrl: 'https://www.milan.ie/'
 };
